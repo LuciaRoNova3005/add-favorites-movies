7f77122017b336b5e3353bd2c5af31025b5531ee
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-22 18:50:56 +0200

    Url fecth

--- src/js/01-search.js
+++ src/js/01-search.js
@@ -1,6 +1,12 @@
 searchButton.addEventListener("click", handlerSearch);
 
 function handlerSearch() {
-  const url = search.value;
+  const wordSearch = search.value.toLowerCase();
+  const url = `http://api.tvmaze.com/search/shows?q=${wordSearch}`;
   console.log(url);
+  fetch(url)
+    .then((result) => result.json())
+    .then((series) => {
+      console.log(series);
+    });
 }
