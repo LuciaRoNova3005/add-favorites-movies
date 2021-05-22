a53bec7df1a3902fff29e86a39307ce417164db9
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-22 18:07:27 +0200

    Add constant and event search

---
+++ src/js/00-constans.js
@@ -0,0 +1,5 @@
+"use strict";
+
+console.log(">> Ready :)");
+const button = document.querySelector("js-button");
+const search = document.querySelector("js-search");
---
+++ src/js/01-search.js
@@ -0,0 +1,4 @@
+button.addEventListener("click");
+
+
+function ();
