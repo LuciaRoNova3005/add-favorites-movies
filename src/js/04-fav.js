497bd27cfbdb41c11e0abdb9f0a0f6657183e4e1
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-10-22 13:27:57 +0200

    change variable color and font main

--- src/js/04-fav.js
+++ src/js/04-fav.js
@@ -1,3 +1,6 @@
+//Si el array que guarda la informacion del local tiene contenido me ejecutas Recupero los datos y lo pinto
+
+//Funcion que escucha click en las peliculas y en las peliculas de favoritas con la js-shows//
 function addListenShow() {
   const cardShows = document.querySelectorAll(".js-shows");
   for (const card of cardShows) {
@@ -5,6 +8,7 @@ function addListenShow() {
   }
 }
 function handleClickFav(event) {
+  // Identificar la li pulsada
   const selectCardFav = event.currentTarget;
 
   const filmId = parseInt(selectCardFav.dataset.id);
