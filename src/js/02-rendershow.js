8559de3a58bc9e9a00c1fdc53986cb91e4814701
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-10-25 10:36:32 +0200

    add mobile list movies styles

--- src/js/02-rendershow.js
+++ src/js/02-rendershow.js
@@ -11,11 +11,11 @@ function renderShows() {
       listHtml += `<li data-id="${titleshow.show.id}" class="js-shows show"> <i class="far fa-thumbs-up"></i>`;
     }
     if (titleshow.show.image !== null) {
-      listHtml += `<img src="${titleshow.show.image.medium}" alt="${titleshow.show.name}">`;
+      listHtml += `<img class="imagen" src="${titleshow.show.image.medium}" alt="${titleshow.show.name}">`;
     } else {
-      listHtml += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="${titleshow.show.name}">`;
+      listHtml += `<img class="imagen" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="${titleshow.show.name}">`;
     }
-    listHtml += `<h2>${titleshow.show.name}</h2>`;
+    listHtml += `<h2 class="tittle2">${titleshow.show.name}</h2>`;
     listHtml += `</li>`;
   }
   showcontainer.innerHTML = listHtml;
