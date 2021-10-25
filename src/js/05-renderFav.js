5265d80f48391bf04d4db5d1654eea1cb53e27c4
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-10-25 12:51:54 +0200

    add section favorite styles

--- src/js/05-renderFav.js
+++ src/js/05-renderFav.js
@@ -1,16 +1,15 @@
 function renderFavorites() {
   let listFavorite = "";
   for (const favfilm of arrayFavorite) {
-    listFavorite += `<li class="fav js-shows"  data-id="${favfilm.show.id}">`;
+    listFavorite += `<li class="fav">`;
 
     if (favfilm.show.image !== null) {
-      listFavorite += `<img class="imagenFav"src="${favfilm.show.image.medium}" alt="${favfilm.show.name}" height="150px">`;
+      listFavorite += `<img class="imagenFav" src="${favfilm.show.image.medium}" alt="${favfilm.show.name}" height="150px">`;
     } else {
-      listFavorite += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="${favfilm.show.name}" height="150px">`;
+      listFavorite += `<img class="imagenFav" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="${favfilm.show.name}" height="150px">`;
     }
     listFavorite += `<h2 class="fav-tittle2">${favfilm.show.name}</h2>`;
-    listFavorite += ` <i class=" fas fa-trash-alt"</i>`;
-
+    listFavorite += ` <i data-id="${favfilm.show.id}"  class="js-shows fas fa-trash-alt"></i>`;
     listFavorite += `</li>`;
   }
   favoritecontainer.innerHTML = listFavorite;
