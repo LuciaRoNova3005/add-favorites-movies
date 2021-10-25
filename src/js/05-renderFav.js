b739eebf8bc56f51f5d2767e51c13026d1602c84
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-10-25 11:49:15 +0200

    fixed container of listfav

--- src/js/05-renderFav.js
+++ src/js/05-renderFav.js
@@ -1,15 +1,16 @@
 function renderFavorites() {
   let listFavorite = "";
   for (const favfilm of arrayFavorite) {
-    listFavorite += `<li class="fav">`;
+    listFavorite += `<li class="fav js-shows"  data-id="${favfilm.show.id}">`;
 
     if (favfilm.show.image !== null) {
-      listFavorite += `<img src="${favfilm.show.image.medium}" alt="${favfilm.show.name}" height="150px">`;
+      listFavorite += `<img class="imagenFav"src="${favfilm.show.image.medium}" alt="${favfilm.show.name}" height="150px">`;
     } else {
       listFavorite += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="${favfilm.show.name}" height="150px">`;
     }
-    listFavorite += `<h2>${favfilm.show.name}</h2>`;
-    listFavorite += `<div data-id="${favfilm.show.id}"  class="js-shows container-delete"><p class="text">Elimina</p></div>`;
+    listFavorite += `<h2 class="fav-tittle2">${favfilm.show.name}</h2>`;
+    listFavorite += ` <i class=" fas fa-trash-alt"</i>`;
+
     listFavorite += `</li>`;
   }
   favoritecontainer.innerHTML = listFavorite;
