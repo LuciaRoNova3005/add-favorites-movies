4626c811efe99b500cf38e16c553b1cced0da726
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-10-26 09:47:56 +0200

    add renderShowNotFound function

---
+++ src/js/08-noresults.js
@@ -0,0 +1,6 @@
+function renderShowNotFound() {
+  let notFoundHtml = "";
+  notFoundHtml += `<h2 class="title3">Lo sentimos, no se ha encontrado ninguna serie</h2>`;
+  notFoundHtml += `<i class="far fa-frown"></i>`;
+  showNotFound.innerHTML = notFoundHtml;
+}
