8cdeeba98de25c063d884348c2d6942d6af2c5f6
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-10-26 09:40:38 +0200

    add const shownotFound

--- src/js/00-constans.js
+++ src/js/00-constans.js
@@ -3,6 +3,7 @@ const searchButton = document.querySelector(".js-button");
 const search = document.querySelector(".js-search");
 const showcontainer = document.querySelector(".js-showlist");
 const favoritecontainer = document.querySelector(".js-favoritelist");
+const showNotFound = document.querySelector(".js-shownotfound");
 let arrayShows = [];
 let arrayFavorite = [];
 let arrayLocal = [];
