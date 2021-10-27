52371d6bbe5c2a235afc3cb543c87cd0ef64ce90
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-10-27 13:07:42 +0200

    add const deleteBtn

--- src/js/00-constans.js
+++ src/js/00-constans.js
@@ -5,6 +5,7 @@ const showcontainer = document.querySelector(".js-showlist");
 const favoritecontainer = document.querySelector(".js-favoritelist");
 const showNotFound = document.querySelector(".js-shownotfound");
 const NotFoundContainer = document.querySelector("js-notFoundContainer");
+const deleteBtn = document.querySelector(".js-delete");
 let arrayShows = [];
 let arrayFavorite = [];
 let arrayLocal = [];
